c699deb32ce70c42aaf326e278d9c000045236b7
Mina Sameh <mina.sameh.lameh@gmail.com>
2025-04-15 14:54:23 +0200

    fix: rebase with latest updates

--- components/refresh-prevention-dialog.tsx
+++ components/refresh-prevention-dialog.tsx
@@ -4,10 +4,15 @@ import { useEffect } from "react";
 import { useTimerStore } from "@/store/timer-store";
 
 export function RefreshPreventionDialog() {
-    const { isRunning, activeIssue } = useTimerStore();
+    const { issues, activeIssueId } = useTimerStore();
 
     useEffect(() => {
         const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+            const isRunning = issues.some(issue => issue.isRunning);
+            const activeIssue = issues.find(issue => issue.id === activeIssueId);
+
+            // If there is an active issue and the timer is running, show the dialog
+            // This will trigger the browser's default confirmation dialog
             if (isRunning && activeIssue) {
                 e.preventDefault();
                 e.returnValue = "";
@@ -17,7 +22,7 @@ export function RefreshPreventionDialog() {
 
         window.addEventListener("beforeunload", handleBeforeUnload);
         return () => window.removeEventListener("beforeunload", handleBeforeUnload);
-    }, [isRunning, activeIssue]);
+    }, [issues, activeIssueId]);
 
     // This component doesn't render anything
     return null;
